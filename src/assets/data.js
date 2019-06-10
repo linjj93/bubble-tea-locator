3040b5092ff516385c91ad3013a83b5ae8a55e8d
linjj93 <linjj93@hotmail.com>
2019-06-10 17:14:45 +0800

    Store data of shops, write calcDistance function

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,4 +1,4 @@
-const locationsToChoose = [
+const userLocation = [
   {
     id: "NE5",
     name: "Clarke Quay MRT",
@@ -36,56 +36,64 @@ const locationsToChoose = [
     longitude: 103.8506241
   }
 ];
-
+const brands = ["Gong Cha", "Koi", "LiHo", "Tiger Sugar"];
 const shops = [
   {
     id: "koi-rafflescity",
+    logo: "Koi",
     name: "Koi Express Raffles City",
     latitude: 1.293418,
     longitude: 103.851561
   },
   {
     id: "koi-citylink",
+    logo: "Koi",
     name: "Koi Citylink Mall",
     latitude: 1.2928431,
     longitude: 103.8508764
   },
   {
     id: "gongcha-rafflesplace",
+    logo: "Gong Cha",
     name: "Gong Cha Raffles Place",
     latitude: 1.2844844,
     longitude: 103.8488933
   },
   {
     id: "liho-clarkequay",
+    logo: "Liho",
     name: "Liho The Central",
     latitude: 1.2891413,
     longitude: 103.8447874
   },
   {
     id: "liho-fareastsquare",
+    logo: "Liho",
     name: "LiHo Far East Square",
     latitude: 1.2831054,
     longitude: 103.8461523
   },
   {
     id: "liho-onerafflesplace",
+    logo: "Liho",
     name: "Liho One Raffles Place",
     latitude: 1.2844078,
     longitude: 103.8487931
   },
   {
     id: "tiger-chinatown",
+    logo: "Tiger Sugar",
     name: "Tiger Sugar Chinatown Point",
     latitude: 1.2894218,
     longitude: 103.8441285
   },
   {
     id: "tiger-capitolpiazza",
+    logo: "Tiger Sugar",
     name: "Tiger Sugar Capitol",
     latitude: 1.2933627,
     longitude: 103.8497737
   }
 ];
 
-export { locationsToChoose, shops };
+export { brands, userLocation, shops };
