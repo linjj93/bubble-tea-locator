3eec0dc173aa1396c0fac8b860566348e5bc08aa
linjj93 <linjj93@hotmail.com>
2019-06-11 10:18:55 +0800

    Add default view when there is no selection made

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -2,22 +2,22 @@ import React from "react";
 import "../styles/Listing.css";
 
 function Listing(props) {
-  // const thereIsNoSelection = !props.nearestShops.length;
-  // if (thereIsNoSelection) {
-  //   return (
-  //     <React.Fragment>
-  //       <ul className="outlet-wrapper">
-  //         <li>
-  //           <span>Shop</span>
-  //           <span>Location</span>
-  //           <span>Distance (in metres)</span>
-  //         </li>
-  //         <p>Select your craving, location or both!</p>
-  //       </ul>
-  //       )
-  //     </React.Fragment>
-  //   );
-  // }
+  const thereIsNoSelection = !props.nearestShops.length;
+  if (thereIsNoSelection) {
+    return (
+      <React.Fragment>
+        <ul className="outlet-wrapper">
+          <li>
+            <span>Shop</span>
+            <span>Location</span>
+            <span>Distance (in metres)</span>
+          </li>
+          <p>Select your craving, location or both!</p>
+        </ul>
+        )
+      </React.Fragment>
+    );
+  }
 
   const listOfShops = props.nearestShops.map(shop => {
     return (
