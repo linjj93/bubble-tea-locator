eaacc3356dfc7e3e49f793f0d054bae27703fb26
linjj93 <linjj93@hotmail.com>
2019-06-12 20:46:10 +0800

    Refactor conditional rendering

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -2,103 +2,32 @@ import React from "react";
 import "../styles/Listing.css";
 
 function Listing(props) {
-  const thereIsNoSelection = !props.nearestShops.length;
-  if (thereIsNoSelection) {
-    return (
-      <React.Fragment>
-        <ul className="outlet-wrapper">
-          <li>
-            <span>Shop</span>
-            <span>Location</span>
-            <span>Distance (in km)</span>
-          </li>
-          <p>Select the store you like and your location!</p>
-        </ul>
-      </React.Fragment>
-    );
-  }
-
-  const filterMaxPerStore = props.showSubsetOfMaxPerStore;
-  if (filterMaxPerStore) {
-    const listOfShopsFiltered = props.subsetOfMaxPerStore
-      .filter(shop => shop.toDisplay)
-      .map(shop => {
-        return (
-          <li key={shop.id}>
-            <span>
-              <img src={shop.logo} alt={shop.brand} />
-            </span>
+  function populateListing(props) {
+    const thereIsNoSelection = !props.nearestShops.length;
+    if (thereIsNoSelection) {
+      return <p>Select the store you like and your location!</p>;
+    }
+    const listOfShops = props.nearestShops.map(shop => {
+      return (
+        <li key={shop.id}>
+          <span>
+            <img src={shop.logo} alt={shop.brand} />
+          </span>
+          <span class="address">
             <span>{shop.name}</span>
-            <span className={shop.distanceMarker}>
-              {isNaN(shop.distanceFromOrigin)
-                ? "Select Location First"
-                : shop.distanceFromOrigin}
-            </span>
-          </li>
-        );
-      });
-    return (
-      <React.Fragment>
-        <ul className="outlet-wrapper">
-          <li>
-            <span>Shop</span>
-            <span>Location</span>
-            <span>Distance (in km)</span>
-          </li>
-          {listOfShopsFiltered}
-        </ul>
-      </React.Fragment>
-    );
+            <span>{shop.unit}</span>
+          </span>
+          <span>{shop.queueTime}</span>
+          <span className={shop.distanceMarker}>
+            {isNaN(shop.distanceFromOrigin)
+              ? "Select Location First"
+              : shop.distanceFromOrigin}
+          </span>
+        </li>
+      );
+    });
+    return listOfShops;
   }
-
-  const filterNearestShopsByNumber = props.showSubsetOfTopN;
-  if (filterNearestShopsByNumber) {
-    const listOfShopsFiltered = props.nearestShops
-      .filter((shop, index) => index + 1 <= props.subsetOfTopN)
-      .map(shop => {
-        return (
-          <li key={shop.id}>
-            <span>
-              <img src={shop.logo} alt={shop.brand} />
-            </span>
-            <span>{shop.name}</span>
-            <span className={shop.distanceMarker}>
-              {isNaN(shop.distanceFromOrigin)
-                ? "Select Location First"
-                : shop.distanceFromOrigin}
-            </span>
-          </li>
-        );
-      });
-    return (
-      <React.Fragment>
-        <ul className="outlet-wrapper">
-          <li>
-            <span>Shop</span>
-            <span>Location</span>
-            <span>Distance (in km)</span>
-          </li>
-          {listOfShopsFiltered}
-        </ul>
-      </React.Fragment>
-    );
-  }
-
-  const listOfShops = props.nearestShops.map(shop => {
-    return (
-      <li key={shop.id}>
-        <span>
-          <img src={shop.logo} alt={shop.brand} />
-        </span>
-        <span>{shop.name}</span>
-        <span className={shop.distanceMarker}>
-          {isNaN(shop.distanceFromOrigin)
-            ? "Select Location First"
-            : shop.distanceFromOrigin}
-        </span>
-      </li>
-    );
-  });
 
   return (
     <React.Fragment>
@@ -106,9 +35,10 @@ function Listing(props) {
         <li>
           <span>Shop</span>
           <span>Location</span>
+          <span>Average Waiting Time</span>
           <span>Distance (in km)</span>
         </li>
-        {listOfShops}
+        {populateListing(props)}
       </ul>
     </React.Fragment>
   );
