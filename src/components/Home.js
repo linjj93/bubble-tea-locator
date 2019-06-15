c248aab174ea42ada7d4f5722a8e839fdf279966
linjj93 <linjj93@hotmail.com>
2019-06-16 00:08:36 +0800

    Update Home Page with more categories

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 function Home(props) {
   return (
     <div data-testid="home-page">
-      <label htmlFor="category-select">Looking for...</label>
-      {/* <select defaultValue="Select a category" id="category-select">
-        <option>Select a category</option>
-        <option>BBT</option>
-      </select> */}
-      <div>
-        <Link to="/bubble-tea">Bubble Tea</Link>
+      <p className="prompt">Looking for...</p>
+      <div className="category-wrapper">
+        <div className="category">
+          <Link className="label" to="/bubble-tea">
+            Bubble Tea
+          </Link>
+        </div>
+        <div className="category">
+          <Link className="label" to="/bargain-shop">
+            ValuDollar/ABC Shops
+          </Link>
+        </div>
+        <div className="category">
+          <Link className="label" to="/mala">
+            Mala Hotpot/Grilled Fish
+          </Link>
+        </div>
       </div>
     </div>
   );
