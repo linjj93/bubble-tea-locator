09095402c662b3786ddc45360d2cf1b6eae85628
linjj93 <linjj93@hotmail.com>
2019-06-16 11:32:28 +0800

    Add pagenotfound and pageunderconstruction

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PageNotFound() {
+  return <div data-testid="page-not-found">Page Not Found</div>;
+}
+
+export default PageNotFound;
---
+++ src/components/PageUnderConstruction.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PageUnderConstruction() {
+  return <div data-testid="under-construction">Under Construction!</div>;
+}
+
+export default PageUnderConstruction;
