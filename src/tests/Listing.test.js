91360b7f390eefbcea9d66f16a922b31e8fe57c1
linjj93 <linjj93@hotmail.com>
2019-06-16 11:33:16 +0800

    Remove unnecessary fireEvent

--- src/tests/Listing.test.js
+++ src/tests/Listing.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Listing from "../components/Listing";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/react/cleanup-after-each";
 import "jest-dom/extend-expect";
 import { shops } from "../assets/shops";
