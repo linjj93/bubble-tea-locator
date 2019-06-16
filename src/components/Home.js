83cd0845d8ab4ebbfde04666ace4ccffd2e3c5c8
linjj93 <linjj93@hotmail.com>
2019-06-16 11:00:17 +0800

    Add new homepage which allow users to pick category

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,23 +5,17 @@ import "../styles/Home.css";
 function Home(props) {
   return (
     <div data-testid="home-page">
-      <p className="prompt">Looking for...</p>
+      <p className="prompt">Pick a category</p>
       <div className="category-wrapper">
-        <div className="category">
-          <Link className="label" to="/bubble-tea">
-            Bubble Tea
-          </Link>
-        </div>
-        <div className="category">
-          <Link className="label" to="/bargain-shop">
-            ValuDollar/ABC Shops
-          </Link>
-        </div>
-        <div className="category">
-          <Link className="label" to="/mala">
-            Mala Hotpot/Grilled Fish
-          </Link>
-        </div>
+        <Link className="category" to="/bubble-tea">
+          Bubble Tea
+        </Link>
+        <Link className="category" to="/bargain-shop">
+          ValuDollar/ABC Shops
+        </Link>
+        <Link className="category" to="/mala">
+          Arcades
+        </Link>
       </div>
     </div>
   );
