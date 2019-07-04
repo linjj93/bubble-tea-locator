2edc462d5b8e3eb3a10318466468d11a04cea7c7
linjj93 <linjj93@hotmail.com>
2019-07-05 00:12:07 +0800

    Set up Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/NavBar.css";
+import axios from "axios";
+const host = "http://localhost:3001";
 
-function NavBar(props) {
-  const { loggedInUser } = props;
-  return (
-    <nav>
-      <ul>
-        <Link
-          to={{
-            pathname: "/dashboard",
-            state: {
-              loggedInUser
-            }
-          }}
-        >
-          Dashboard
-        </Link>
-        <Link to="/find-a-shop">Find a Shop</Link>
-        <Link to="/">Logout</Link>
-      </ul>
-    </nav>
-  );
+class NavBar extends React.Component {
+  handleLogout() {
+    sessionStorage.removeItem("jwt");
+    axios
+      .post(`${host}/users/logout`)
+      .then(res => {
+        console.log("logout");
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { loggedInUser } = this.props;
+    return (
+      <nav>
+        <div className="mobile-nav">
+          <span>Menu</span>
+          <div className="links">
+            <Link
+              className="nav-item"
+              to={{
+                pathname: "/dashboard",
+                state: {
+                  loggedInUser
+                }
+              }}
+            >
+              Dashboard
+            </Link>
+
+            <Link className="nav-item" to="/find-a-shop">
+              Find a Shop
+            </Link>
+
+            <Link
+              onClick={this.handleLogout.bind(this)}
+              className="nav-item"
+              to="/"
+            >
+              Logout
+            </Link>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
