d21f3211926d9e6ec04361a18f0a57e5c839c2cd
linjj93 <linjj93@hotmail.com>
2019-07-04 20:52:19 +0800

    Modify routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,32 @@
 import React from "react";
 import Login from "./Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Home";
+import Dashboard from "./Dashboard";
 import BubbleTeaLocator from "./BubbleTeaLocator";
 import Tracker from "./Tracker";
 import PageNotFound from "./PageNotFound";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/home" component={Home} />
-        <Route path="/find-a-shop" component={BubbleTeaLocator} />
-        <Route path="/drinking-history" component={Tracker} />
-        <Route component={PageNotFound} />
-      </Switch>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/find-a-shop" component={BubbleTeaLocator} />
+          <Route path="/drinking-history" component={Tracker} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
